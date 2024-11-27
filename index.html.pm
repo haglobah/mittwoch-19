ab255eea9f3c17b2c76551fa9d5a133d7f551d39
lehaas <lennart.haas@outlook.com>
2024-11-27 22:17:44 +0100

    Fix layout and css.

--- index.html.pm
+++ index.html.pm
@@ -4,9 +4,9 @@
   ◊h1[#:class "text-5xl md:text-6xl font-bold text-center"]{
     Lust auf:
   }
-  ◊div[#:class "mx-8 flex flex-col gap-4"]{
+  ◊div[#:class "mx-8 flex flex-col gap-8"]{
     ◊braced[#:href "#Projekte"]{
-      gemeinsam an Projekten arbeiten
+      Projekte
     }
     ◊braced[#:href "#Lernen"]{
       Lernen
@@ -17,70 +17,76 @@
   }
 }
 
-◊sec[#:title "Projekte"]{
-  ◊div[#:class "flex flex-row flex-wrap my-4"]{
-    ◊div[#:class "basis-1/3"]{
-      ◊img[#:src "assets/"]{
+◊div[#:class "px-4"]{
+  ◊sec[#:title "Projekte"]{
+    ◊div[#:class "flex flex-row flex-wrap my-8"]{
+      ◊div[#:class "md:basis-1/3"]{
+        ◊img[#:src "/assets/stock_image.jpg" #:class "object-cover h-full w-full rounded-xl"]{
 
+        }
+      }
+      ◊div[#:class "md:basis-2/3 md:pl-16 my-4"]{
+        ◊par{
+          Das sind einige von uns beim coden (ja, wir haben Spaß).
+        }
+        ◊par{
+          Wir sind Menschen, die ziemlich gerne 
+        }
+        ◊par{
+          Gleichzeitig sind am Mittwoch-19 viele Menschen in der Westspitze, die absolut mal wieder Lust darauf haben, ihre LinA-1 Kenntnisse zu benutzen.
+        }
+        ◊par{
+          Deswegen geht tk am Mittwoch-19 in die Westspitze, um gemeinsam mit anderen an ihren Zetteln zu rechnen. Das macht ihr mehr Spaß; und sie lernt auch schneller.
+        }
       }
     }
-    ◊div[#:class "basis-2/3"]{
-      ◊par{
-        Das ist ....
+
+  }
+  ◊sec[#:title "Lernen"]{
+    ◊div[#:class "flex flex-row-reverse flex-wrap my-8"]{
+      ◊div[#:class "rounded md:basis-1/3"]{
+        ◊img[#:src "/assets/stock_image.jpg" #:class "object-cover h-full w-full rounded-xl"]{
+
+        }
       }
-      ◊par{
-      }
-      ◊par{
-      }
-      ◊par{
+      ◊div[#:class "md:basis-2/3 md:pr-16 my-4"]{
+        ◊par{
+          Das ist tk.
+        }
+        ◊par{
+          tk möchte nicht alleine an ihren LinA-Zetteln rechnen.
+        }
+        ◊par{
+          Gleichzeitig sind am Mittwoch-19 viele Menschen in der Westspitze, die absolut mal wieder Lust darauf haben, ihre LinA-1 Kenntnisse zu benutzen.
+        }
+        ◊par{
+          Deswegen geht tk am Mittwoch-19 in die Westspitze, um gemeinsam mit anderen an ihren Zetteln zu rechnen. Das macht ihr mehr Spaß; und sie lernt auch schneller.
+        }
       }
     }
   }
-}
-◊sec[#:title "Lernen"]{
-  ◊div[#:class "flex flex-row-reverse flex-wrap my-4"]{
-    ◊div[#:class "basis-1/3"]{
-      ◊img[#:src "assets/"]{
+  ◊sec[#:title "Spaß"]{
+    ◊div[#:class "flex flex-row flex-wrap my-8"]{
+      ◊div[#:class "md:basis-1/3"]{
+        ◊img[#:src "/assets/stock_image.jpg" #:class "object-cover h-full w-full rounded-xl"]{
 
+        }
+      }
+      ◊div[#:class "md:basis-2/3 md:pl-16 mt-4"]{
+        ◊par{
+          Das sind einige von uns beim coden (ja, wir haben Spaß).
+        }
+        ◊par{
+          Wir sind Menschen, die ziemlich gerne 
+        }
+        ◊par{
+          Gleichzeitig sind am Mittwoch-19 viele Menschen in der Westspitze, die absolut mal wieder Lust darauf haben, ihre LinA-1 Kenntnisse zu benutzen.
+        }
+        ◊par{
+          Deswegen geht tk am Mittwoch-19 in die Westspitze, um gemeinsam mit anderen an ihren Zetteln zu rechnen. Das macht ihr mehr Spaß; und sie lernt auch schneller.
+        }
       }
     }
-    ◊div[#:class "basis-2/3"]{
-      ◊par{
-        Das ist tk.
-      }
-      ◊par{
-        tk möchte nicht alleine an ihren LinA-Zetteln rechnen.
-      }
-      ◊par{
-        Gleichzeitig sind am Mittwoch-19 viele Menschen in der Westspitze, die absolut mal wieder Lust darauf haben, ihre LinA-1 Kenntnisse zu benutzen.
-      }
-      ◊par{
-        Deswegen geht tk am Mittwoch-19 in die Westspitze, um gemeinsam mit anderen an ihren Zetteln zu rechnen. Das macht ihr mehr Spaß; und sie lernt auch schneller.
-      }
-    }
+
   }
-}
-◊sec[#:title "Spaß"]{
-  ◊div[#:class "flex flex-row flex-wrap my-4"]{
-    ◊div[#:class "basis-1/3"]{
-      ◊img[#:src "assets/"]{
-
-      }
-    }
-    ◊div[#:class "basis-2/3"]{
-      ◊par{
-        Das sind einige von uns beim coden (ja, wir haben Spaß).
-      }
-      ◊par{
-        Wir sind Menschen, die ziemlich gerne 
-      }
-      ◊par{
-        Gleichzeitig sind am Mittwoch-19 viele Menschen in der Westspitze, die absolut mal wieder Lust darauf haben, ihre LinA-1 Kenntnisse zu benutzen.
-      }
-      ◊par{
-        Deswegen geht tk am Mittwoch-19 in die Westspitze, um gemeinsam mit anderen an ihren Zetteln zu rechnen. Das macht ihr mehr Spaß; und sie lernt auch schneller.
-      }
-    }
-  }
-
 }
--- pollen.rkt
+++ pollen.rkt
@@ -5,10 +5,10 @@
 (provide (all-defined-out))
 
 (define (braced #:href href . elems)
-  `(a ([class "flex justify-center gap-2 text-4xl md:text-5xl font-bold"] [href ,href])
-      (span "{")
-      (span ,@elems)
-      (span "}")))
+  `(a ([class "flex justify-center gap-4 text-3xl md:text-5xl items-center "] [href ,href])
+      (span ([class "text-7xl md:text-8xl"]) "{")
+      (span ([class "font-bold text-center"]) ,@elems)
+      (span ([class "text-7xl md:text-8xl"]) "}")))
 
 (define (sec #:title title . elems)
   `(div ([id ,title] [class "my-8 mx-6"])
@@ -19,3 +19,4 @@
 (define (par . elems)
   `(p ([class "text-lg my-2"])
     ,@elems))
+
